1fcf0392d8656795cb435e43880ca6fe4944d367
Vansh-Baghel <vanshbaghel912@gmail.com>
2024-03-02 01:02:47 +0530

    Added node server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello World");
+});
+
+app.listen(4000, () => {
+  console.log(`Server running on port 4000`);
+});
